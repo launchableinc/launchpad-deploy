977f0d13ad962e3f3ceb3b941c7cd1772b7f57b4
Kohsuke Kawaguchi <kk@kohsuke.org>
2020-03-21 14:07:27 -0700

    Making this more of all-in-one

--- run.sh
+++ run.sh
@@ -3,7 +3,17 @@ export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 
 set -xe
-aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
-cd kubernetes
-find . -type f -name '*.yaml' | xargs perl -i -p -e "s/\$GITREF/${GITHUB_SHA}/g;" 
-kubectl apply -f .
+# push image
+$(aws ecr get-login --no-include-email --region "$INPUT_AWS_REGION")
+ACCOUNT=$(aws sts get-caller-identity | jq -r '.Account')
+REMOTE=$ACCOUNT.dkr.ecr.$INPUT_AWS_REGION.amazonaws.com/$INPUT_IMAGE:$GITHUB_SHA
+docker tag $INPUT_IMAGE $REMOTE
+docker push $REMOTE
+
+# apply config
+if [ "${GITHUB_REF}" = "refs/heads/master" ]; then
+    aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
+    cd kubernetes
+    find . -type f -name '*.yaml' | xargs perl -i -p -e "s/\$GITREF/${GITHUB_SHA}/g;" 
+    kubectl apply -f .
+fi
